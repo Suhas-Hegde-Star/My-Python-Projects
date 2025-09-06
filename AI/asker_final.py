f1ce0b42a84bd738419420186b45d730f48e346f
Suhas-Hegde-Star <hmsuhas2025@gmail.com>
2025-09-06 21:01:01 +0530

    Full AI C 2

--- AI/asker_final.py
+++ AI/asker_final.py
@@ -5,6 +5,26 @@ def askfinal(wante):
         wante = input("Write the topic you want")
         if wante.lower() == "math":
             asker_math.askmath(wanted = any)
+        elif wante.lower() == "i want to do some math":
+            asker_math.askmath(wanted = any)
+        elif wante.lower() == "do some math":
+            asker_math.askmath(wanted = any)
+        elif wante.lower() == "i want to learn math":
+            asker_math.askmath(wanted = any)
+        elif wante.lower() == "maths":
+            asker_math.askmath(wanted = any)
+        elif wante.lower() == "i wanna to do some math":
+            asker_math.askmath(wanted = any)
+        elif wante.lower() == "i wanna learn math":
+            asker_math.askmath(wanted = any)
+        elif wante.lower() == "i wanna do some tabala":
+            pass
+        elif wante.lower() == "do some tabala":
+            pass
+        elif wante.lower() == "i want to do some tabala":
+            pass
+        elif wante.lower() == "i want to learn tabala":
+            pass
         elif wante.lower() == "tabala":
             pass
         else:
